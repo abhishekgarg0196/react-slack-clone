282f05f13a6d1a4743e59fcbe303b53a07c23e73
abhishek <abhishek.garg0196@gmail.com>
2020-09-15 21:39:14 +0530

    Add sidebar, maincontainer

---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div id="sidebar">
+        <div className="user-profile">
+          <div>
+            <img />
+          </div>
+          <div>Ronald</div>
+        </div>
+
+        <div className="user-channels"></div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
